6eee69dc0e58db0ce930df884c81d0b98cc22c00
bashkirtsevich <bashkirtsevich@gmail.com>
2017-03-27 22:48:26 +0500

    Set constant local node id

--- demo_query.py
+++ demo_query.py
@@ -19,13 +19,13 @@ def bootstrapDone(found, server, key):
         reactor.stop()
     else:
         print "Bootstrap completed"
-        
+
     server.get(key).addCallback(done)
 
 
 key = from_hex_to_byte('f7bf674bd41c5a7affc8a61479d8968063fc609d')
 
-server = Server()
+server = Server(id=from_hex_to_byte('b481586aac12255d290fc575656dd31d67f765b8'))
 server.listen(12346)
 server.bootstrap([('67.215.246.10', 6881)]).addCallback(bootstrapDone, server, key)
 
