71ccbe9541bc9b2644135faa0fc3cc9a034fbda9
bashkirtsevich <bashkirtsevich@gmail.com>
2017-03-29 22:45:42 +0500

    Add host resolving before startup udp-server

--- demo_query.py
+++ demo_query.py
@@ -12,7 +12,7 @@ def done(result):
     reactor.stop()
 
 
-def bootstrapDone(found, server, info_hash):
+def bootstrap_done(found, server, info_hash):
     if len(found) == 0:
         print "Could not connect to the bootstrap server."
         reactor.stop()
@@ -22,10 +22,13 @@ def bootstrapDone(found, server, info_hash):
         server.get_peers(info_hash).addCallback(done)
 
 
-info_hash = from_hex_to_byte('f7bf674bd41c5a7affc8a61479d8968063fc609d')
+def start_dht_server(ip):
+    info_hash = from_hex_to_byte('f7bf674bd41c5a7affc8a61479d8968063fc609d')
 
-server = Server(id=from_hex_to_byte('b481586aac12255d290fc575656dd31d67f765b8'))
-server.listen(12346)
-server.bootstrap([('67.215.246.10', 6881)]).addCallback(bootstrapDone, server, info_hash)
+    server = Server(id=from_hex_to_byte('b481586aac12255d290fc575656dd31d67f765b8'))
+    server.listen(12346)
+    server.bootstrap([(ip, 6881)]).addCallback(bootstrap_done, server, info_hash)
 
+
+reactor.resolve('router.bittorrent.com').addCallback(start_dht_server)
 reactor.run()
